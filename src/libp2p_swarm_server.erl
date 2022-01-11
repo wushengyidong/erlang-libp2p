0b263108977c782ffd20287899f817aea3f96e65
andy zhao <286505491@qq.com>
2022-01-11 14:25:55 +0800

    revert

--- src/libp2p_swarm_server.erl
+++ src/libp2p_swarm_server.erl
@@ -75,15 +75,10 @@ handle_info({handle_identify, Session, {ok, Identify}}, State=#state{tid=TID}) -
     PeerBook = libp2p_swarm:peerbook(TID),
     try libp2p_peerbook:put(PeerBook, [libp2p_identify:peer(Identify)]) of
         ok ->
-            OUR_IP = "/p2p/112BsKd6XFTDCERpAceQpuuLpudC2WL2CpEbZBMtswcBujb4vxBj",
-            case Addr  of
-                OUR_IP ->
-                  lager:warning("BB: Close session for addr ~p", [Addr]),
-                  libp2p_session:close(Session);
-                _ ->
-                    libp2p_config:insert_session(TID, Addr, Session),
-                    libp2p_peerbook:register_session(PeerBook, Session, Identify)
-            end;
+            libp2p_config:insert_session(TID,
+                                         Addr,
+                                         Session),
+            libp2p_peerbook:register_session(PeerBook, Session, Identify);
         {error, Reason} ->
             lager:warning("Failed to put peerbook entry for ~p ~p", [Addr, Reason]),
             libp2p_session:close(Session)
